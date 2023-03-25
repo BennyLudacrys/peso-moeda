f687f1164b2a506cecf1c73eb9f0b70017cf01e5
benny ludacrys <bennedito01@gmail.com>
2023-03-25 23:19:10 +0200

    adicao de mais escopo

--- src/Main.java
+++ src/Main.java
@@ -19,18 +19,27 @@ public class Main {
                 group2Sum = coins[4] + coins[5] + coins[6] + coins[7];
                 group3Sum = coins[8] + coins[9] + coins[10]+ coins[11];
 
-                if (group1Sum == group2Sum){
-                    int newgroup1 = coins[0]+ coins[1];
-                    int newgroup2= coins[8] + coins[9];
-
-                    if (newgroup1 != newgroup2){
-                        if (coins[8] == coins[0]){
+                //se ha equilibrio entre goup1 e group2
+                if (group1Sum == group2Sum) {
+                    int newgroup1 = coins[0] + coins[1];
+                    int newgroup2 = coins[8] + coins[9];
+                    int newgroup3 = coins[10] + coins[11];
+                    //se houver um desequilibrio na segunda pesagem
+                    if (newgroup1 != newgroup2) {
+                        if (coins[8] == coins[0]) {
                             System.out.println("a moeda falsa e 10");
-                        }else {
+                        } else {
                             System.out.println("a moeda falsa e 9");
                         }
-
+                        //se houver equilibrio na segunda pesagem
+                    }else {
+                        if (coins[0]+coins[10]==newgroup1){
+                            System.out.println("a moeda falsa e 12");
+                        }else {
+                            System.out.println("a moeda falsa e 11");
+                        }
                     }
+
                     //vamos continuar de acordo com o video
                 } else if (group1Sum>=group2Sum) {
                     int newgroup1 = coins[4]+ coins[5];
